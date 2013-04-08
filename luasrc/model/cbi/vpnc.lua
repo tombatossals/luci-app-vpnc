08a5fa0d29b58041ba0a722a878bcc0563be234c
David Rubert <david.rubert@gmail.com>
2013-04-08 20:38:02 +0000

    The form shows the config file values

--- luasrc/model/cbi/vpnc.lua
+++ luasrc/model/cbi/vpnc.lua
@@ -15,18 +15,33 @@ $Id: vpnc.lua 6984 2011-04-13 15:14:42Z soma $
 
 m = Map("vpnc", translate("VPNC"))
 
-s = m:section(TypedSection, "vpnc", "VPNC")
+s = m:section(TypedSection, "general", translate("VPNC General Settings"))
 s.anonymous = true
 
 s:tab("general",  translate("General Settings"))
+s:tab("template",  translate("Template"))
 
 s:taboption("general", Value, "gateway", translate("Cisco VPN server hostname"))
-s:taboption("general", Value, "id", translate("Group ID"))
+s:taboption("general", Value, "groupid", translate("Group ID"))
 s:taboption("general", Value, "secret", translate("Group Password"))
 s:taboption("general", Value, "username", translate("Username"))
-s:taboption("general", Value, "password", translate("Password"))
+pw = s:taboption("general", Value, "password", translate("Password"))
+pw.password = true
 
 s = m:section(TypedSection, "status", translate("VPNC status"))
 s.anonymous = true
+start = s:option(Button, "start", translate("Start"))
+start.inputstyle = "apply"
+start.write = function(self, section)
+        luci.sys.call("/etc/init.d/vpnc %s >/dev/null" %{ self.option })
+end
+
+stop = s:option(Button, "stop", translate("Stop"))
+stop.inputstyle = "remove"
+stop.write = start.write
+
+function m.on_commit(map)
+	# Write file
+end
 
 return m
